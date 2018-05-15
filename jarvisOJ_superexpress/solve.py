cdbf1dfafab2f43e21f7ef999e1ad7878e73fc97
M4xW4n9 <aboultraman@gmail.com>
2018-05-15 23:19:12 +0800

    affine cipher

--- jarvisOJ_superexpress/solve.py
+++ jarvisOJ_superexpress/solve.py
@@ -7,13 +7,13 @@ from string import printable
 crypt = '''805eed80cbbccb94c36413275780ec94a857dfec8da8ca94a8c313a8ccf9'''
 crypt = [int(crypt[i: i + 2], 16) for i in xrange(0, len(crypt), 2)]
 
-for a in xrange(251):
-    for b in xrange(251):
-        if (ord('T') * a + b) % 251 == crypt[0] and (ord('W') * a + b) % 251 == crypt[1]:
+for x in xrange(251):
+    for y in xrange(251):
+        if (ord('T') * x + y) % 251 == crypt[0] and (ord('W') * x + y) % 251 == crypt[1]:
             flag = ""
             for c in crypt:
                 for i in printable:
-                    if (ord(i) * a + b) % 251 == c:
+                    if (ord(i) * x + y) % 251 == c:
                         flag += i
 
             print flag
